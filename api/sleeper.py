cc7cee27203158ee8f432069e8a15255199426fb
Manish Koleti <koletimanish@utexas.edu>
2025-10-18 11:57:30 -0500

    changes to daily update

--- api/sleeper.py
+++ api/sleeper.py
@@ -10,40 +10,50 @@ class Sleeper:
     def __init__(self):
         self.base_url = "https://api.sleeper.app/v1"
 
-    def update_players(self):
+    def daily_update(self):
         response = requests.get(f"{self.base_url}/players/nfl")
         df = pd.DataFrame()
         for player_id in response.json():
             player_info = response.json()[player_id]
-            if(player_info["sport"] == "nfl"):
+            if(player_info.get("sport") != "nfl" and player_info.get("status") != "active"):
+                continue
+            
+            if(player_info.get("position") == 'DEF'):
                 player_data = {
                     "id": player_id,
-                    "first_name": player_info["first_name"],
-                    "last_name": player_info["last_name"],
-                    "position": player_info["position"],
-                    "status": player_info["status"],
-                    "fantasy_data_id": player_info["fantasy_data_id"],
-                    "espn_id": player_info["espn_id"],
-                    "yahoo_id": player_info["yahoo_id"],
+                    "first_name": player_info.get("first_name", ""),
+                    "last_name": player_info.get("last_name", ""),
+                    "position": player_info.get("position", ""),
+                    "status": "",
+                    "fantasy_data_id": "",
+                    "espn_id": "",
+                    "yahoo_id": "",
                 }
-                df = df.append(player_data, ignore_index=True)
-                break
-            
+            else:
+                player_data = {
+                    "id": player_id,
+                    "first_name": player_info.get("first_name", ""),
+                    "last_name": player_info.get("last_name", ""),
+                    "position": player_info.get("position", ""),
+                    "status": player_info.get("status", ""),
+                    "fantasy_data_id": player_info.get("fantasy_data_id", ""),
+                    "espn_id": player_info.get("espn_id", ""),
+                    "yahoo_id": player_info.get("yahoo_id", ""),
+                }
+            df = pd.concat([df, pd.DataFrame([player_data])], ignore_index=True)
+            print(f"Dataframe now has {len(df)} elements after adding {player_data['first_name']} {player_data['last_name']}")
         # Change to project root directory and save pickle file in data folder
-        os.chdir(get_project_root())
+        os.chdir("..")
         os.makedirs("data", exist_ok=True)
         df.to_pickle("data/players.pkl")
         return df
 
     def get_players(self):
-        print(get_project_root())
-        print(os.getcwd())
-        os.chdir(get_project_root())
-        print(os.getcwd())
-        # df = pd.read_pickle("data/players.pkl")
-        # return df
+        os.chdir("..")
+        df = pd.read_pickle("data/players.pkl")
+        return df
 
 if __name__ == "__main__":
-    sleeper = Sleeper()
-    # sleeper.update_players()
-    print(sleeper.get_players())
+    sleeper_api = Sleeper()
+    sleeper_api.daily_update()
+    # print(sleeper_api.get_players())
